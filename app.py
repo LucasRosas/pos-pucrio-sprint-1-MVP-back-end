6606c5100c542aa4722ede5ba2680bce14147039
Lucas Rosas <lucasrosas@outlook.com>
2025-03-28 19:42:16 -0300

    atualiza a documentação da função get_schedules para especificar que o mês deve estar entre 1 e 12

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ def get_schedules(query: ScheduleSchemaSearch):
     """Faz a busca por todas as reservas cadastradas no mês e ano informados. O token é utilizado para verificar se o usuário é o dono da reserva.
     
     **Args:**
-        query (ScheduleSchemaSearch): dicionário contendo month (int) e year (int) e token (int) do usuário.
+        query (ScheduleSchemaSearch): dicionário contendo month (int) entre 1 e 12, inclusive, year (int) e token (int) do usuário.
 
     **Returns:**
         dict: Representação da listagem de reservas.
